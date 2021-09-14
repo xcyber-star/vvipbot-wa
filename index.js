22a9342aef7b550aa59245d825961242cc552641
Xcyber_roy <64800088+xcyber-star@users.noreply.github.com>
2021-09-14 15:11:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Roy\n' // Nama kamu
+            + 'ORG:Roy-Bot;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6282277121630:+62 822-7712-1630\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
